1efcdee21e124bf95e3a30dd91a0a0e0a4c6f9ab
ianporpino1 <ianporpino127@gmail.com>
2024-03-08 16:01:03 -0300

    Serial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String fileName = "src/data/data.csv";
+        List<Double> x_knownList = new ArrayList<>();
+        List<Double> y_knownList = new ArrayList<>();
+        List<Double> z_knownList = new ArrayList<>();
+
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            br.readLine();
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                x_knownList.add(Double.parseDouble(parts[0]));
+                y_knownList.add(Double.parseDouble(parts[1]));
+                z_knownList.add(Double.parseDouble(parts[2]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        double[] x_known = x_knownList.stream().mapToDouble(Double::doubleValue).toArray();
+        double[] y_known = y_knownList.stream().mapToDouble(Double::doubleValue).toArray();
+        double[] z_known = z_knownList.stream().mapToDouble(Double::doubleValue).toArray();
+
+        double[] x_unknown = {1.5, 2.5, 3.5};
+        double[] y_unknown = {1.5, 2.5, 3.5};
+
+
+        double[] z_interpolated = SpatialInterpolation.inverseDistanceWeighting(x_known, y_known, z_known, x_unknown, y_unknown, 2.0);
+
+        for (double val : z_interpolated) {
+            System.out.println(val);
+        }
     }
 }
--- src/SpatialInterpolation.java
+++ src/SpatialInterpolation.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class SpatialInterpolation {
+public class SpatialInterpolation {
+
+    public static double[] inverseDistanceWeighting(double[] x_known, double[] y_known, double[] z_known, double[] x_unknown, double[] y_unknown, double power) {
+        double[] z_unknown = new double[x_unknown.length];
+        for (int i = 0; i < x_unknown.length; i++) {
+            double sumWeights = 0.0;
+            double sumWeightedValues = 0.0;
+            for (int j = 0; j < x_known.length; j++) {
+                double distance = Math.sqrt(Math.pow(x_known[j] - x_unknown[i], 2) + Math.pow(y_known[j] - y_unknown[i], 2));
+                double weight = 1.0 / Math.pow(distance, power);
+                sumWeights += weight;
+                sumWeightedValues += weight * z_known[j];
+            }
+            z_unknown[i] = sumWeightedValues / sumWeights;
+        }
+        return z_unknown;
+    }
 }
