9618eb1d26c3f2c146d7b82ce4a919108407a5ae
ianporpino1 <ianporpino127@gmail.com>
2024-03-11 11:47:40 -0300

    Serial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -7,37 +6,54 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String fileName = "src/data/data.csv";
-        List<Double> x_knownList = new ArrayList<>();
-        List<Double> y_knownList = new ArrayList<>();
-        List<Double> z_knownList = new ArrayList<>();
+        String fileKnownPoints = "src/data/known_points.csv";
+        List<Double> x_known = new ArrayList<>();
+        List<Double> y_known = new ArrayList<>();
+        List<Double> z_known = new ArrayList<>();
+        //long startTime = System.nanoTime();
 
-
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileKnownPoints))) {
             br.readLine();
             String line;
             while ((line = br.readLine()) != null) {
                 String[] parts = line.split(",");
-                x_knownList.add(Double.parseDouble(parts[0]));
-                y_knownList.add(Double.parseDouble(parts[1]));
-                z_knownList.add(Double.parseDouble(parts[2]));
+                x_known.add(Double.parseDouble(parts[0]));
+                y_known.add(Double.parseDouble(parts[1]));
+                z_known.add(Double.parseDouble(parts[2]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String fileUnknownPoints = "src/data/unknown_points.csv";
+        List<Double> x_unknown = new ArrayList<>();
+        List<Double> y_unknown = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileUnknownPoints))) {
+            br.readLine();
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                x_unknown.add(Double.parseDouble(parts[0]));
+                y_unknown.add(Double.parseDouble(parts[1]));
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        double[] x_known = x_knownList.stream().mapToDouble(Double::doubleValue).toArray();
-        double[] y_known = y_knownList.stream().mapToDouble(Double::doubleValue).toArray();
-        double[] z_known = z_knownList.stream().mapToDouble(Double::doubleValue).toArray();
-
-        double[] x_unknown = {1.5, 2.5, 3.5};
-        double[] y_unknown = {1.5, 2.5, 3.5};
 
 
-        double[] z_interpolated = SpatialInterpolation.inverseDistanceWeighting(x_known, y_known, z_known, x_unknown, y_unknown, 2.0);
+        long startTime = System.nanoTime(); //conta somente o tempo do algoritmo
 
-        for (double val : z_interpolated) {
-            System.out.println(val);
-        }
+        List<Double> z_interpolated = SpatialInterpolation.inverseDistanceWeighting(x_known, y_known, z_known, x_unknown, y_unknown, 2.0);
+
+        long endTime = System.nanoTime();
+
+        double  duration = (endTime - startTime) / 1e9; //com 1000 pontos desconhecidos e 40 milhoes de pontos conhecidos, 7min
+
+        System.out.println("Tempo de execução: " + duration + " segundos");
+
+       for (double val : z_interpolated) {
+           System.out.println(val);
+       }
     }
 }
--- src/SpatialInterpolation.java
+++ src/SpatialInterpolation.java
@@ -1,17 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SpatialInterpolation {
 
-    public static double[] inverseDistanceWeighting(double[] x_known, double[] y_known, double[] z_known, double[] x_unknown, double[] y_unknown, double power) {
-        double[] z_unknown = new double[x_unknown.length];
-        for (int i = 0; i < x_unknown.length; i++) {
+    public static List<Double> inverseDistanceWeighting(List<Double> x_known, List<Double> y_known, List<Double> z_known, List<Double> x_unknown, List<Double> y_unknown, double power) {
+        List<Double> z_unknown = new ArrayList<>(x_unknown.size());
+        for (int i = 0; i < x_unknown.size(); i++) {
             double sumWeights = 0.0;
             double sumWeightedValues = 0.0;
-            for (int j = 0; j < x_known.length; j++) {
-                double distance = Math.sqrt(Math.pow(x_known[j] - x_unknown[i], 2) + Math.pow(y_known[j] - y_unknown[i], 2));
+            for (int j = 0; j < x_known.size(); j++) {
+                double distance = Math.sqrt(Math.pow(x_known.get(j) - x_unknown.get(i), 2) + Math.pow(y_known.get(j) - y_unknown.get(i), 2));
                 double weight = 1.0 / Math.pow(distance, power);
                 sumWeights += weight;
-                sumWeightedValues += weight * z_known[j];
+                sumWeightedValues += weight * z_known.get(j);
             }
-            z_unknown[i] = sumWeightedValues / sumWeights;
+            z_unknown.add(sumWeightedValues / sumWeights);
         }
         return z_unknown;
     }
