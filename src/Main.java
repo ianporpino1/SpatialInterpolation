900259b75c80683a0fad969945c3df7b9e70b2d2
ianporpino1 <ianporpino127@gmail.com>
2024-04-17 12:04:12 -0300

    added Points

--- src/Main.java
+++ src/Main.java
@@ -7,35 +7,33 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         String fileKnownPoints = "src/data/known_points.csv";
-        List<Double> x_known = new ArrayList<>();
-        List<Double> y_known = new ArrayList<>();
-        List<Double> z_known = new ArrayList<>();
-
-        readPoints(fileKnownPoints, x_known, y_known, z_known);
+        List<Point> known_points = new ArrayList<>();
+        readPoints(fileKnownPoints, known_points, true);
 
         String fileUnknownPoints = "src/data/unknown_points.csv";
-        List<Double> x_unknown = new ArrayList<>();
-        List<Double> y_unknown = new ArrayList<>();
+        List<Point> unknown_points = new ArrayList<>();
+        readPoints(fileUnknownPoints, unknown_points,false);
 
-        readPoints(fileUnknownPoints, x_unknown, y_unknown, null);
+        List<Point> z_interpolated = SpatialInterpolation.inverseDistanceWeighting(known_points, unknown_points, 2.0);
 
-        List<Double> z_interpolated = SpatialInterpolation.inverseDistanceWeighting(x_known, y_known, z_known, x_unknown, y_unknown, 2.0);
-
-       for (double val : z_interpolated) {
+       for (Point val : z_interpolated) {
            System.out.println(val);
        }
     }
-    public static void readPoints(String filePath, List<Double> xList, List<Double> yList, List<Double> zList) {
+    public static void readPoints(String filePath, List<Point> points, Boolean flag) {
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             br.readLine();
             String line;
             while ((line = br.readLine()) != null) {
                 String[] parts = line.split(",");
-                xList.add(Double.parseDouble(parts[0]));
-                yList.add(Double.parseDouble(parts[1]));
-                if (zList != null && parts.length > 2) {
-                    zList.add(Double.parseDouble(parts[2]));
+                Point point;
+                if(flag){
+                    point = new Point(Double.parseDouble(parts[0]), Double.parseDouble(parts[1]),Double.parseDouble(parts[2]));
                 }
+                else {
+                    point = new Point(Double.parseDouble(parts[0]), Double.parseDouble(parts[1]),null);
+                }
+                points.add(point);
             }
         } catch (IOException e) {
             e.printStackTrace();
