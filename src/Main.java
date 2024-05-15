4ab8632ea73acfa8cff6892a08ff00f68f6d7b31
ianporpino1 <ianporpino127@gmail.com>
2024-05-15 10:58:41 -0300

    bug fix

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.concurrent.*;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
+    public static void main(String[] args) {
         long startTime = System.nanoTime();
         
         String fileKnownPoints = "src/data/known_points.csv";
