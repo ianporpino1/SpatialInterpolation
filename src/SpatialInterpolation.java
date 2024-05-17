edea249680699fef46385cd6bdcea7b38271513f
ianporpino1 <ianporpino127@gmail.com>
2024-05-17 17:28:17 -0300

    update Point para Class

--- src/SpatialInterpolation.java
+++ src/SpatialInterpolation.java
@@ -5,8 +5,8 @@ public class SpatialInterpolation {
     
     public List<Double> inverseDistanceWeighting(List<Point> known, Point unknown, double power) {
         List<Double> weights = new ArrayList<>();
-        Double sumWeights = 0.0;
-        Double sumWeightedValues = 0.0;
+        double sumWeights = 0.0;
+        double sumWeightedValues = 0.0;
         for (Point k : known) {
             Double distance = Math.sqrt(Math.pow(k.x() - unknown.x(), 2) + Math.pow(k.y() - unknown.y(), 2));
             Double weight = 1.0 / Math.pow(distance, power);
